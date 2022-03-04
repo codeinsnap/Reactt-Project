16e1324a8ffb047542707d342053af74cab10f06
Yogesh <yog192325@gmail.com>
2022-03-04 23:36:10 +0530

    Initial phase 04-03-2022

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer-container"> 
+            <div className="footer-block">
+                <div className='footer-left-block'>
+                    <h1> Home </h1>
+                </div>
+
+                <div className='footer-right-block'>
+                    <h1> Contact Us </h1>
+                    <h1> Login </h1>
+                    <h1> Projects </h1>
+                    <h1> Profile  </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/components/Header/header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header-Title"> 
+            <div className='header-Container'>
+                <div className='header-Container-left'>
+                    <h1> Home </h1>
+                </div>
+
+                <div className='header-Container-Right'>
+                    <h1> Contact Us </h1>
+                    <h1> Login </h1>
+                    <h1> Projects </h1>
+                    <h1> Profile  </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Header;
