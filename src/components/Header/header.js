deb3452e7cabe55fc5971b67600bd750ff871b65
Yogesh <yog192325@gmail.com>
2022-03-05 22:00:47 +0530

    search 5-3-22

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from './search';
 
 function Header() {
     return (
@@ -7,6 +8,7 @@ function Header() {
                 <div className='header-Container-left'>
                     <h1> Home </h1>
                 </div>
+                <Search />
 
                 <div className='header-Container-Right'>
                     <h1> Contact Us </h1>
